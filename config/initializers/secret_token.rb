f91e5108f955caec2b5c09692c584916fde04e4b
diegoachury <tiran_o@hotmail.com>
2013-02-02 11:35:00 -0500

    iniciando repositorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sbuga::Application.config.secret_token = 'b92d735a3d5254dd973cd5b58047c0d1fe651a42d4524ad1caa8fedc04122b35d3e9a490fed65c346669b6e4fd9ffbd332ba75bfed1b37166539712f1caeca1d'
